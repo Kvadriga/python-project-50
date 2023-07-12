68c01dd25d6ebe208bc346caa83afa370989b0c6
Evgeniy Gordienko <evg.gordienko@gmail.com>
2023-07-12 08:09:34 +0300

    step 3.5

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,15 +2,23 @@
 
 
 import argparse
+import json
 
 
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+parser.add_argument('-f', '--format', help='set format of output')
 parser.add_argument('first_file')
 parser.add_argument('second_file')
 
 args = parser.parse_args()
 
 def main():
-    print('!!')
+    with open(args.first_file) as handle:
+        file1 = json.loads(handle.read())
+    with open(args.second_file) as handle:
+        file2 = json.loads(handle.read())
+    print('\nstart\n')
+    print(file1, file2)
+
 
 
