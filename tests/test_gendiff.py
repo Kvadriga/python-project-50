84b16dafe000417cc8584288dc8cd56b924983e9
Evgeniy Gordienko <evg.gordienko@gmail.com>
2023-07-27 08:25:54 +0300

    11

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,8 +3,13 @@
 
 from gendiff.scripts.gendiff import generate_diff
 
+
+file1 = 'tests/file1.json'
+file2 = 'tests/file2.json'
+
 def test_flat_json():
-    assert generate_diff('file1.json file2.json') == '''{
+    diff = generate_diff(file1, file2)
+    assert diff == '''{
  - follow: False
    host: hexlet.io
  - proxy: 123.234.53.22
